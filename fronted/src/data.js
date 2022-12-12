c87166c9603f63812dc979f35313035eeaea9541
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2022-12-11 23:16:10 -0600

    Lista de productos

---
+++ fronted/src/data.js
@@ -0,0 +1,55 @@
+const data = {
+    products:[
+        {
+            name: `Nike Slim Shirt`,
+            slug: `nike-slim-shirt`,
+            category: `Shirts`,
+            image: "https://res.cloudinary.com/ddfzagwob/image/upload/v1670811416/amazona/p1_y7rfn4.jpg",
+            price: 120,
+            countInStock: 10,
+            brand: `Nike`,
+            rating: 4.5, 
+            numReviews: 10,
+            description: `High quality shirt`
+        },
+        {
+            name: `Adidas Fit Shirt`,
+            slug: `adidas-fit-shirt`,
+            category: `Shirts`,
+            image: `https://res.cloudinary.com/ddfzagwob/image/upload/v1670811416/amazona/p2_ekqltv.jpg`,
+            price: 250,
+            countInStock: 20,
+            brand: `Adidas`,
+            rating: 4.5, 
+            numReviews: 14,
+            description: `High quality shirt`
+        },
+        {
+            name: `Nike Slim Pant`,
+            slug: `nike-slim-pant`,
+            category: `Shirts`,
+            image: `https://res.cloudinary.com/ddfzagwob/image/upload/v1670811416/amazona/p3_laffdl.jpg`,  //679x829
+            price: 25,
+            countInStock: 15,
+            brand: `Nike`,
+            rating: 4.5, 
+            numReviews: 14,
+            description: `High quality shirt`
+        },
+        {
+            name: `Adidas Fit Pant`,
+            slug: `adidas-fit-pant`,
+            category: `Pants`,
+            image: `https://res.cloudinary.com/ddfzagwob/image/upload/v1670811416/amazona/p4_w7lnm1.jpg`,
+            price: 65,
+            countInStock: 5,
+            brand: `Adidas`,
+            rating: 4.5, 
+            numReviews: 10,
+            description: `High quality shirt`
+        }
+        
+    ]
+}
+
+export default data;
